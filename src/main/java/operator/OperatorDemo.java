6e1e7d97ae453182631572c2270fb6c97fba472a
Shigal <shiglaws@gmail.com>
2022-11-06 14:01:05 +0530

    bitwise and logical operator explanined

---
+++ src/main/java/operator/OperatorDemo.java
@@ -0,0 +1,15 @@
+package operator;/*
+ *
+ * @author Lawshiga
+ *
+ */
+
+public class OperatorDemo {
+    public static void main(String[] args) {
+
+        int a = 3; // 0011
+        int b = 9; // 1001
+        //bitwise AND
+        System.out.println("a&b = " + (a & b)); // 0001
+    }
+}
