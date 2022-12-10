b6dfc5f5b0afed75238ee93859359813d8304d47
Shigal <shiglaws@gmail.com>
2022-12-10 10:41:24 +0530

    static and abstract conflict

---
+++ src/main/java/revision/StaticAbstract.java
@@ -0,0 +1,12 @@
+package revision;/*
+ *
+ * @author Lawshiga
+ *
+ */
+
+public abstract class StaticAbstract {
+    // abstract static method func
+    // it has no body
+//    abstract static void func();
+    // Illegal combination of modifiers: 'abstract' and 'static'
+}
